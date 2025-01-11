a0f9701a5682e3ab70a96df9c255a294ed195e55
Gokhan Kus <gkus1998@hotmail.com>
2025-01-11 13:30:58 +0300

    Product.cs, ProductFeature.cs

---
+++ API/ElasticSearch/ElasticSearch.API/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ElasticSearch.API.Models
+{
+	public class Product
+	{
+		//elastic default olarak Id'yi string atar o yuzden string Id ve bu Id response'da bulunmaz metadata'da bulunur
+		[JsonPropertyName("_id")]
+		public string Id { get; set; } = null!; 
+		public string Name { get; set; } = null!;
+		public decimal Price { get; set; }
+		public int Stock { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime? Updated { get; set; }
+		public ProductFeature? Feature { get; set; }
+	}
+}
