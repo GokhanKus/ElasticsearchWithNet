154577394da41774a1ef583a4b3f122ba7c8ec76
Gokhan Kus <gkus1998@hotmail.com>
2025-01-11 12:57:15 +0300

    ElasticSearchExtension & Configuration

---
+++ API/ElasticSearch/ElasticSearch.API/Extensions/ElasticSearchExt.cs
@@ -0,0 +1,17 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+
+namespace ElasticSearch.API.Extensions
+{
+	public static class ElasticSearchExt
+	{
+		public static void AddElasticExt(this IServiceCollection services, IConfiguration configuration)
+		{
+			var elasticUrl = configuration.GetSection("Elastic")["Url"];
+			var pool = new SingleNodePool(new Uri(elasticUrl!));
+			var settings = new ElasticsearchClientSettings(pool);
+			var client = new ElasticsearchClient(settings);
+			services.AddSingleton(client); //elastic firmasinin önerisi ioc kaydinin singleton olarak yapilmasi.
+		}
+	}
+}
--- API/ElasticSearch/ElasticSearch.API/Program.cs
+++ API/ElasticSearch/ElasticSearch.API/Program.cs
@@ -1,7 +1,8 @@
+using ElasticSearch.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,7 +10,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddElasticExt(builder.Configuration);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
