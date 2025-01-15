982ad3d66f1b4d3d2d9e5f3fb624f91b3e76ded6
Gokhan Kus <gkus1998@hotmail.com>
2025-01-15 12:39:33 +0300

    Term Query 2

--- API/ElasticSearch/ElasticSearch.API/Repositories/ECommerceRepository.cs
+++ API/ElasticSearch/ElasticSearch.API/Repositories/ECommerceRepository.cs
@@ -10,8 +10,18 @@ namespace ElasticSearch.API.Repositories
 		private const string indexName = "kibana_sample_data_ecommerce";
 		public async Task<ImmutableList<ECommerce>> TermQuery(string customerFirstName)
 		{
+			#region 1st way, 3rd way
+			//1st way
+			//var results = await _elasticClient.SearchAsync<ECommerce>(s => s.Index(indexName)
+			//	.Query(q => q.Term(t => t.Field("customer_first_name.keyword").Value(customerFirstName)))); //.Size(10) default
+
+			//3rd way
+			//var termQuery = new TermQuery("customer_first_name.keyword") { Value = customerFirstName, CaseInsensitive = true };
+			//var result = await _elasticClient.SearchAsync<ECommerce>(s => s.Index(indexName).Query(termQuery));
+			#endregion
+			//2nd way (type safe)
 			var results = await _elasticClient.SearchAsync<ECommerce>(s => s.Index(indexName)
-				.Query(q => q.Term(t => t.Field("customer_first_name.keyword").Value(customerFirstName)))); //.Size(10) default
+			.Query(q => q.Term(t => t.Field(f => f.CustomerFirstName.Suffix("keyword")).Value(customerFirstName))));
 
 			foreach (var hit in results.Hits)
 				hit.Source.Id = hit.Id!;
