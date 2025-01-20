0e8179dc9b064c8952171ebf40e99a99bdad5a94
Gokhan Kus <gkus1998@hotmail.com>
2025-01-20 21:55:15 +0300

    ECommerceController.cs and SearchPageViewModel.cs

---
+++ API/ElasticSearch/BlogApp.Web/Controllers/ECommerceController.cs
@@ -0,0 +1,22 @@
+﻿using BlogApp.Web.Services;
+using BlogApp.Web.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Web.Controllers
+{
+	public class ECommerceController(ECommerceService _eCommerceService) : Controller
+	{
+		public async Task<IActionResult> Search([FromQuery] SearchPageViewModel searchPageView)
+		{
+			var (eCommerceList,totalItemCount, pageLinkCount) 
+				= await _eCommerceService.SearchAsync(searchPageView.SearchViewModel, searchPageView.Page, searchPageView.PageSize);
+
+			searchPageView.List = eCommerceList;
+			searchPageView.TotalItemCount = totalItemCount;
+			searchPageView.PageLinkCount = pageLinkCount;
+
+
+			return View();
+		}
+	}
+}
